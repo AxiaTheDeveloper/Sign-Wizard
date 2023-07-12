976278ac2c5e6314a5eb14c06b6dc0c58694836c
AxiaTheDeveloper <101692512+AxiaTheDeveloper@users.noreply.github.com>
2023-07-12 18:43:11 +0700

    aaaaaa new

--- Assets/Script/UI/SubmitPotionUI.cs
+++ Assets/Script/UI/SubmitPotionUI.cs
@@ -35,7 +35,7 @@ public class SubmitPotionUI : MonoBehaviour
         Selected_On();
     }
     public void Show_AreYouSure(string potionName){
-        text_AreYouSure.text = "Apakah aku akan kirim "+ potionName + " ini ?";
+        text_AreYouSure.text = "Apakah aku akan kirim potion ini ?";
         UI_AskingWhichPotion.SetActive(false);
         UI_AreYouSure.SetActive(true);
     }
